c1dd2c9ecadc5f5655946f74a22ab10137a87a00
hosonu <hosonoyuki07@gmail.com>
2026-05-01 11:58:10 +0200

    feat: export WaveletTransformer from package root

--- src/bci/__init__.py
+++ src/bci/__init__.py
@@ -7,10 +7,12 @@ from bci.csp import CustomCSP
 from bci.data import epochs_to_Xy
 from bci.features import LogVarianceTransformer
 from bci.pipeline import make_motor_imagery_pipeline
+from bci.wavelet import WaveletTransformer
 
 __all__ = [
     "CustomCSP",
     "LogVarianceTransformer",
     "epochs_to_Xy",
     "make_motor_imagery_pipeline",
+    "WaveletTransformer",
 ]
